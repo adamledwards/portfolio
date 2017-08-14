39b8e4799ca0b06cac14e0a2fd139bc33013d02c
Adam Edwards <adamedwards@Adams-MacBook-Pro-2.local>
2017-08-14 09:07:02 +0100

    Adding ./DS_Store to gitignore

--- app/Editor/components/BlockRenderer/blockRendererFn.js
+++ app/Editor/components/BlockRenderer/blockRendererFn.js
@@ -2,11 +2,12 @@
 import Hero from '~/modules/Hero'
 import ProjectInfo from '~/modules/ProjectInfo'
 import Text from '~/modules/Text'
+import blockShallowCheck from '~/utils/blockShallowCheck.js'
 
 const Components = {
-  Hero,
-  ProjectInfo,
-  Text
+  Hero: blockShallowCheck(Hero),
+  ProjectInfo: blockShallowCheck(ProjectInfo),
+  Text: blockShallowCheck(Text)
 }
 
 export default function blockRendererFn (block: string): React$Component<*> {
